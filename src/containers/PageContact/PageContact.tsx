d4c377153d23fd8aae700aac2bae960c2e4d2630
dxnkramer <dankramer0@me.com>
2022-02-05 21:47:14 +0000

    Revert "Merge pull request #1 from dxnkramer/Contact_Form"
    This reverts commit ac4f5df5adfb3f25415351975fbb6f3483b165bd, reversing
    changes made to 5272dd22c3b5bbc048722ad9fd51a560788f95dc.

--- src/containers/PageContact/PageContact.tsx
+++ src/containers/PageContact/PageContact.tsx
@@ -7,7 +7,6 @@ import SocialsList from "components/SocialsList/SocialsList";
 import Textarea from "components/Textarea/Textarea";
 import { Helmet } from "react-helmet";
 import SectionSubscribe2 from "components/SectionSubscribe2/SectionSubscribe2";
-import emailjs from '@emailjs/browser';
 
 export interface PageContactProps {
   className?: string;
@@ -29,82 +28,68 @@ const info = [
 ];
 
 const PageContact: FC<PageContactProps> = ({ className = "" }) => {
-
-  const sendEmail = (e : any) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_yklfd0i', 'template_kmllxcy', e.target, 'user_Y42cWwZi6lpaoh6FhJy9n')
-        .then((result) => {
-          console.log(result.text);
-        }, (error) => {
-          console.log(error.text);
-        });
-    e.target.reset();
-  };
-
   return (
-      <div className={`nc-PageContact ${className}`} data-nc-id="PageContact">
-        <Helmet>
-          <title>Contact | CryptoWall</title>
-        </Helmet>
-        <LayoutPage
-            subHeading="Drop us message and we will get back for you."
-            headingEmoji=""
-            heading="Contact us"
-        >
-          <div className="grid gap-8 lg:grid-cols-2">
-            <div className="max-w-sm space-y-6">
-              {info.map((item, index) => (
-                  <div key={index}>
-                    <h3 className="uppercase font-semibold text-sm dark:text-neutral-200 tracking-wider">
-                      {item.title}
-                    </h3>
-                    <span className="block mt-2 text-neutral-500 dark:text-neutral-400">
+    <div className={`nc-PageContact ${className}`} data-nc-id="PageContact">
+      <Helmet>
+        <title>Contact | CryptoWall</title>
+      </Helmet>
+      <LayoutPage
+        subHeading="Drop us message and we will get back for you."
+        headingEmoji=""
+        heading="Contact us"
+      >
+        <div className="grid gap-8 lg:grid-cols-2">
+          <div className="max-w-sm space-y-6">
+            {info.map((item, index) => (
+              <div key={index}>
+                <h3 className="uppercase font-semibold text-sm dark:text-neutral-200 tracking-wider">
+                  {item.title}
+                </h3>
+                <span className="block mt-2 text-neutral-500 dark:text-neutral-400">
                   {item.desc}
                 </span>
-                  </div>
-              ))}
-              <div>
-                <h3 className="uppercase font-semibold text-sm dark:text-neutral-200 tracking-wider">
-                  🌏 SOCIALS
-                </h3>
-                <SocialsList className="mt-2" />
               </div>
-            </div>
-            <div className="border border-neutral-100 dark:border-neutral-700 lg:hidden"></div>
+            ))}
             <div>
-              <form className="grid grid-cols-1 gap-6" onSubmit={sendEmail}>
-                <label className="block">
-                  <Label>Full name</Label>
-
-                  <Input placeholder="Example Doe" type="text" className="mt-1" name="name"/>
-                </label>
-                <label className="block">
-                  <Label>Email address</Label>
-
-                  <Input
-                      type="email"
-                      placeholder="example@example.com"
-                      className="mt-1"
-                      name="email"
-                  />
-                </label>
-                <label className="block">
-                  <Label>Message</Label>
-
-                  <Textarea className="mt-1" rows={6}  name="message"/>
-                </label>
-                <ButtonPrimary type="submit">Send Message</ButtonPrimary>
-              </form>
+              <h3 className="uppercase font-semibold text-sm dark:text-neutral-200 tracking-wider">
+                🌏 SOCIALS
+              </h3>
+              <SocialsList className="mt-2" />
             </div>
           </div>
-        </LayoutPage>
+          <div className="border border-neutral-100 dark:border-neutral-700 lg:hidden"></div>
+          <div>
+            <form className="grid grid-cols-1 gap-6" action="#" method="post">
+              <label className="block">
+                <Label>Full name</Label>
 
-        {/* OTHER SECTIONS */}
-        <div className="container pb-16 lg:pb-28">
-          <SectionSubscribe2 />
+                <Input placeholder="Example Doe" type="text" className="mt-1" />
+              </label>
+              <label className="block">
+                <Label>Email address</Label>
+
+                <Input
+                  type="email"
+                  placeholder="example@example.com"
+                  className="mt-1"
+                />
+              </label>
+              <label className="block">
+                <Label>Message</Label>
+
+                <Textarea className="mt-1" rows={6} />
+              </label>
+              <ButtonPrimary type="submit">Send Message</ButtonPrimary>
+            </form>
+          </div>
         </div>
+      </LayoutPage>
+
+      {/* OTHER SECTIONS */}
+      <div className="container pb-16 lg:pb-28">
+        <SectionSubscribe2 />
       </div>
+    </div>
   );
 };
 
