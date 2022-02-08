c7ae580ac36ee16e005e4ba90fadff8e095e1a13
FurqanRaheem <furqanraheem10@gmail.com>
2022-02-08 17:46:41 +0000

    Recommit

--- src/components/SectionSubscribe2/SectionSubscribe2.tsx
+++ src/components/SectionSubscribe2/SectionSubscribe2.tsx
@@ -1,58 +1,80 @@
-import React, { FC } from "react";
+import React, {FC, useState} from "react";
 import ButtonCircle from "components/Button/ButtonCircle";
 import rightImg from "images/SVG-subcribe2.png";
 import NcImage from "components/NcImage/NcImage";
 import Badge from "components/Badge/Badge";
 import Input from "components/Input/Input";
+import { addDoc, collection, serverTimestamp} from "firebase/firestore";
+import { db } from "../../configs/firebase-config";
 
 export interface SectionSubscribe2Props {
   className?: string;
 }
 
 const SectionSubscribe2: FC<SectionSubscribe2Props> = ({ className = "" }) => {
+  const [email, setEmail] = useState("");
+  const [popUpMessage, setPopUpMessage] = useState("");
+
+  const newsLetterCollectionRef = collection(db, "newsletter");
+
+  const processNewsletter = async (e : any) => {
+    e.preventDefault();
+
+    await addDoc(newsLetterCollectionRef, {
+      email,
+      time: serverTimestamp()
+    });
+
+    e.target.reset();
+    setPopUpMessage("Thank you for subscribing!");
+    setTimeout(() => {setPopUpMessage("");}, 3000);
+  };
   return (
-    <div
-      className={`nc-SectionSubscribe2 relative flex flex-col lg:flex-row items-center ${className}`}
-      data-nc-id="SectionSubscribe2"
-    >
-      <div className="flex-shrink-0 mb-14 lg:mb-0 lg:mr-10 lg:w-2/5">
-        <h2 className="font-semibold text-4xl">Join our newsletter 🎉</h2>
-        <span className="block mt-6 text-neutral-500 dark:text-neutral-400">
+      <div
+          className={`nc-SectionSubscribe2 relative flex flex-col lg:flex-row items-center ${className}`}
+          data-nc-id="SectionSubscribe2"
+      >
+        <div className="flex-shrink-0 mb-14 lg:mb-0 lg:mr-10 lg:w-2/5">
+          <h2 className="font-semibold text-4xl">Join our newsletter 🎉</h2>
+          <span className="block mt-6 text-neutral-500 dark:text-neutral-400">
           Never miss out on the latest Web3 news
         </span>
-        <ul className="space-y-5 mt-10">
-          <li className="flex items-center space-x-4">
-            <Badge name="01" />
-            <span className="font-medium text-neutral-700 dark:text-neutral-300">
+          <ul className="space-y-5 mt-10">
+            <li className="flex items-center space-x-4">
+              <Badge name="01" />
+              <span className="font-medium text-neutral-700 dark:text-neutral-300">
               Get discounted membership to CryptoWall
             </span>
-          </li>
-          <li className="flex items-center space-x-4">
-            <Badge color="red" name="02" />
-            <span className="font-medium text-neutral-700 dark:text-neutral-300">
+            </li>
+            <li className="flex items-center space-x-4">
+              <Badge color="red" name="02" />
+              <span className="font-medium text-neutral-700 dark:text-neutral-300">
               Get premium insights from our specialist team
             </span>
-          </li>
-        </ul>
-        <form className="mt-10 relative max-w-sm">
-          <Input
-            required
-            aria-required
-            placeholder="Enter your email"
-            type="email"
-          />
-          <ButtonCircle
-            type="submit"
-            className="absolute transform top-1/2 -translate-y-1/2 right-1"
-          >
-            <i className="las la-arrow-right text-xl"></i>
-          </ButtonCircle>
-        </form>
+            </li>
+          </ul>
+          <form className="mt-10 relative max-w-sm" onSubmit={processNewsletter}>
+            <Input
+                required
+                aria-required
+                placeholder="Enter your email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <ButtonCircle
+                type="submit"
+                className="absolute transform top-1/2 -translate-y-1/2 right-1"
+            >
+              <i className="las la-arrow-right text-xl"></i>
+            </ButtonCircle>
+          </form>
+          {/*TODO: STYLE FOLLOWING*/}
+          {popUpMessage && <div className="newsletter-pop-up" style={{background: "green"}}>Thank you for subscribing.</div>}
+        </div>
+        <div className="flex-grow">
+          <NcImage src={rightImg} />
+        </div>
       </div>
-      <div className="flex-grow">
-        <NcImage src={rightImg} />
-      </div>
-    </div>
   );
 };
 
