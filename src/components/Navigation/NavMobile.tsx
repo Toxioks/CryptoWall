2ff03f798bec830e196797fd8db981d263eca23c
dxnkramer <dankramer0@me.com>
2022-02-09 13:53:51 +0000

    Front End tweaks

--- src/components/Navigation/NavMobile.tsx
+++ src/components/Navigation/NavMobile.tsx
@@ -175,7 +175,7 @@ const NavMobile: React.FC<NavMobileProps> = ({
       </ul>
       <div className="flex items-center justify-between py-6 px-5 space-x-4">
         <a href="/#" target="_blank" rel="noopener noreferrer">
-          <ButtonPrimary>Get Template</ButtonPrimary>
+          <ButtonPrimary>Button</ButtonPrimary>
         </a>
       </div>
     </div>
--- src/containers/PageSingle/SingleContentDemo.tsx
+++ src/containers/PageSingle/SingleContentDemo.tsx
@@ -23,7 +23,7 @@ const SingleContentDemo = () => {
       <figure>
         <img
           src="https://images.unsplash.com/photo-1501493870936-9c2e41625521?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1045&q=80"
-          alt="nc blog"
+          alt="CryptoWall blog"
         />
         <figcaption>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure vel
