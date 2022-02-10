d95baf66a2a92cb7c4ac44639aabc6765e862ef3
dxnkramer <dankramer0@me.com>
2022-02-10 20:37:36 +0000

    Meta tag update

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 dotenvCra.config();
 //
 let plugins = [require("tailwindcss"), require("autoprefixer")];
-// ENABLE IF RUN WITH RTL VERSION
+// ENABLE
 if (process.env.REACT_APP_LRT_OR_RTL === "rtl") {
   plugins[2] = require("postcss-cssjanus");
 }
